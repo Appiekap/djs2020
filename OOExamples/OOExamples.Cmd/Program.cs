65b990d4dc386d0bff1f9a2680f4ad4dcfa5e294
Rick Artz <rick.artz@capgemini.com>
2020-09-10 09:49:06 +0200

    Welcome to OO

--- OOExamples/OOExamples.Cmd/Program.cs
+++ OOExamples/OOExamples.Cmd/Program.cs
@@ -7,6 +7,7 @@ namespace OOExamples.Cmd
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to OO");
         }
     }
 }
