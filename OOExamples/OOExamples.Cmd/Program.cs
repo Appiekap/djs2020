8817a49baa466576804f65ea8ad4b04f7742d64d
Rick Artz <rick.artz@capgemini.com>
2020-09-10 09:39:35 +0200

    First HelloWorld Console Project

---
+++ OOExamples/OOExamples.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOExamples.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
